a6bd27165f702b0cdd04422995671c19acf3bcf9
alex <seewalker.120@gmail.com>
2014-04-17 16:46:58 -0400

    Now with the generic crontab. The old generic crontab branch is now unnecessary.

--- db/harvest-store-generic.pl
+++ db/harvest-store-generic.pl
@@ -1,7 +1,5 @@
 #!/usr/bin/perl
 
-# harvest-store-mills.pl
-#
 # This is not the best way to do this, unfortunately there is something fishy 
 # about the pm8 and how it responds to SNMP get requests.  Using the Net::SNMP
 # module exacerbates the problem, using snmpget from the command line does not, 
@@ -10,10 +8,16 @@
 # 15-March-2011	charliep	Original coding. 
 # 29-March-2011	charliep	Removed password.
 # 08-March-2012	charliep	Re-wrote after stewie disk failure...
-
+# 30-September-2012 alex seewald Re-wrote harvest script to be generic
 use strict;
 use warnings;
 use DBD::Pg;
+
+#these command line arguments come from the crontab
+
+my $building_name = $ARGV[0];
+my $ip = $ARGV[1];
+my $amperage_correction = $ARGV[2];
 
 my $timestamp;
 my $pReal; 
@@ -22,17 +26,11 @@ my $dbh;
 my $sth; 
 
 $timestamp = time; 
-$pReal = `java --cp /home/energy/scripts/production/ ModbusReadDemand 159.28.165.104 31.25`;
+$pReal = `java -cp /home/energy/scripts/production ModbusReadDemand $ip $amperage_correction`;
 chomp($pReal); 
 $pReal =~ s/"//g; 
-# printf("At %d pReal = %d\n",  $timestamp, $pReal); 
 
-$dbh = DBI->connect("dbi:Pg:dbname=$database");
-if (!$dbh) {
-	die "Cannot connect to database $database";
-}
-
-$dbh->do("insert into electrical_energy values ('Mills', $pReal, to_timestamp($timestamp))"); 
-
+$dbh = DBI->connect("dbi:Pg:dbname=$database") or die "Cannot connect to database $database";
+$dbh->do("insert into electrical_energy values ('$building_name', $pReal, to_timestamp($timestamp))"); 
 $dbh->disconnect;
 exit(0); 
